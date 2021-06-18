fe68087e53b11e1326f51e78f24790c0caf9033f
atifbajwa <atifg7828@gmail.com>
2021-06-18 20:58:31 +0500

    List rendring in vue3.js

--- main.js
+++ main.js
@@ -3,7 +3,20 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: "A warm fuzzy pair of socks. hello"
+            image: './assets/images/socks_blue.jpg',
+            description: "A warm fuzzy pair of socks. hello",
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {
+                    id: 1,
+                    color: 'green'
+                },
+                {
+                    id: 2,
+                    color: 'blue'
+                }
+            ]
         }
     }
 })
