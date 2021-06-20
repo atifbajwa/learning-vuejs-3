4f1553ff768963ca20d6f2177410e78e0b1e52a7
atifbajwa <atifg7828@gmail.com>
2021-06-20 16:46:33 +0500

    learn how to create component and props in vue

---
+++ components/ProductDisplay.js
@@ -0,0 +1,72 @@
+app.component('product-display', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: 
+    /*html*/
+    `<div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <img :src="image" alt="">
+      </div>
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+        <p>{{ sale }}</p>
+        <p>Shipping: {{ shipping }}</p>
+        <p v-if="inStock">In stock</p>
+        <p v-else>Out stock</p>
+
+        <product-details :details="details"></product-details>
+        <div
+          class="color-circle"
+          v-for = "(variant, index) in variants"
+          :key="variant.id"
+          :style="{ backgroundColor: variant.color}"
+          @mouseover="updateVariant(index)">  
+        </div>
+        <button class="button" :class="{ disabledButton: !inStock }" :disabled="!inStock" v-on:click="addToCart">Add to Cart</button>
+      </div>
+    </div>
+  </div>`,
+  data() {
+    return {
+        product: 'Socks',
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
+        details: ['50% cotton', '30% wool', '20% polyester'],
+        variants: [
+          { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+          { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+        ]
+    }
+  },
+  methods: {
+      addToCart() {
+          this.cart += 1
+      },
+      updateVariant(index) {
+          this.selectedVariant = index
+      }
+  },
+  computed: {
+      title() {
+          return this.brand + ' ' + this.product
+      },
+      image() {
+          return this.variants[this.selectedVariant].image
+      },
+      inStock() {
+          return this.variants[this.selectedVariant].quantity
+      },
+      shipping() {
+          if (this.premium) {
+              return 'free'
+          }
+          return 2.99
+      }
+
+  }
+})
